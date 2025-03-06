3d917bf88a86955c5bbff3e359bba6e11221c037
Joë <martinezmontoyajoe@gmail.com>
2025-03-06 22:01:34 +0100

    feat(app): normalized
    Reafctored on Users service model
    Creation OK

---
+++ src/Controller/CreatePetController.php
@@ -0,0 +1,49 @@
+<?php
+namespace App\Controller;
+
+use App\Exception\ApiException;
+use App\Service\PetService;
+use App\Utils\ApiResponse;
+use App\Utils\HttpStatusCodes;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CreatePetController extends AbstractController
+{
+    #[Route('/api/pets/create-pet', name: 'create_pet', methods: ['POST'])]
+    public function __invoke(Request $request, PetService $petService): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+        if (! $data) {
+            return ApiResponse::error([
+                "title"   => "Invalid JSON Payload",
+                "detail"  => "The request body is not a valid JSON",
+                "message" => "Invalid data provided",
+            ], HttpStatusCodes::BAD_REQUEST);
+        }
+
+        try {
+            $response = $petService->createPet($data);
+            return ApiResponse::success([
+                "detail"  => "Pet created",
+                "message" => "$response's profile has been successfully created",
+            ], HttpStatusCodes::CREATED);
+        } catch (\Exception $e) {
+            if ($e instanceof ApiException) {
+                return ApiResponse::error([
+                    "title"   => $e->getTitle(),
+                    "detail"  => $e->getDetail(),
+                    "message" => $e->getMessage(),
+                ], $e->getStatusCode());
+            }
+
+            return ApiResponse::error([
+                "title"   => "Unexpected Error",
+                "detail"  => "An unexpected error occurred while creating the user",
+                "message" => $e->getMessage(),
+            ], HttpStatusCodes::SERVER_ERROR);
+        }
+    }
+}
